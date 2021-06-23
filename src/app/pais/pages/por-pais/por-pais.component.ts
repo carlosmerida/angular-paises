976e255b891c94f4212b6223ab9b09e0843c960e
carlosmerida <c.merida.es@gmail.com>
2021-06-23 16:46:00 +0200

    Se termina la app con la sección 10

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,6 +6,11 @@ import { Country } from '../../interfaces/pais.interface';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `
   ]
 })
 export class PorPaisComponent {
@@ -14,9 +19,14 @@ export class PorPaisComponent {
   hayError: boolean = false;
   paises: Country[] = [];
 
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
+
   constructor( private paisService: PaisService ) { }
 
   buscar( termino: string ) {
+    
+    this.mostrarSugerencias = false;
     this.hayError = false;
     this.termino = termino;
 
@@ -32,7 +42,18 @@ export class PorPaisComponent {
 
   sugerencias( termino: string ) {
     this.hayError = false;
-    // TODO: crear sugerencias
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+    
+    this.paisService.buscarPais(termino)
+      .subscribe( 
+        paises => this.paisesSugeridos = paises.splice(0,5),
+        (err) => this.paisesSugeridos = []
+      );
+  }
+
+  buscarSugerido( termino: string ) {
+    this.buscar( termino );
   }
 
 }
